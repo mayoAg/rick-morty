2070d10aa84c44f96b1e709bb55cb34cd9fd30e2
Mayur Garg ala re aala <magarg@tibco.com>
2019-12-15 20:42:38 +0530

    github pages

---
+++ precache-manifest.f9537596ef48ac298f2dfc90a7cbc5d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28167cbc380cb237f15115576b3de487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95e84d02c87435ee81da",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d8887fb1121aa8fb6bf9",
+    "url": "/static/css/main.8445ef1b.chunk.css"
+  },
+  {
+    "revision": "95e84d02c87435ee81da",
+    "url": "/static/js/2.24409664.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.24409664.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8887fb1121aa8fb6bf9",
+    "url": "/static/js/main.c8d7f37f.chunk.js"
+  },
+  {
+    "revision": "a37702bd7f647d04333b",
+    "url": "/static/js/runtime-main.366585f1.js"
+  },
+  {
+    "revision": "5959b4182b142d4e2be19f5f0c51097e",
+    "url": "/static/media/fabric.5959b418.png"
+  }
+]);
