02a44e4312be928c1916e2b763cc962384902d79
Mayur Garg ala re aala <magarg@tibco.com>
2019-12-15 20:58:45 +0530

    github pages deployment

---
+++ precache-manifest.5429ffa81ecee029972a85febd496a51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7f04e0038bd39db5d096d6cff05356",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95e84d02c87435ee81da",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "22069e456cd9e3770122",
+    "url": "./static/css/main.a5e62b0e.chunk.css"
+  },
+  {
+    "revision": "95e84d02c87435ee81da",
+    "url": "./static/js/2.24409664.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.24409664.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22069e456cd9e3770122",
+    "url": "./static/js/main.c8d7f37f.chunk.js"
+  },
+  {
+    "revision": "e93aaf22b44b3dc275cb",
+    "url": "./static/js/runtime-main.348a0ab7.js"
+  },
+  {
+    "revision": "5959b4182b142d4e2be19f5f0c51097e",
+    "url": "./static/media/fabric.5959b418.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9537596ef48ac298f2dfc90a7cbc5d9.js"
+  "./precache-manifest.5429ffa81ecee029972a85febd496a51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
